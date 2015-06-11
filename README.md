1142316253e94173185d7eb1764ea0681ddc31c0
Nitin Pathak <np1810@gmail.com>
2015-06-12 02:22:04 +0530

    Initial Commit
    Initial Commit to "2048 Undo" Repo

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# 2048undo
-2048 with Unlimited Undo Storage ;)
+# 2048 Undo
+This is the <strong>MODDED</strong> version of 2048 that includes unlimited <strong>UNDO,</strong> which stores locally i.e. stays even if you refresh the webpage and it saves only <strong>VALID</strong> moves. You can play it on your phone via [http://np1810.github.io](http://np1810.github.io)
+
+### Screenshot
+<p align="center">
+  <img src="http://nitinpathak.esy.es/pics/2048undo.jpg" alt="Screenshot"/>
+</p>
+
+## Contributing
+Changes and improvements are more than welcome! Feel free to fork and open a pull request.
+
+## License
+2048 is licensed under the [MIT license.](https://github.com/np1810/2048undo/blob/master/LICENSE.txt)
+
+## Donations
+If you would like to donate then [Donate to UNICEF](http://supportunicef.org)
---
+++ js/local_storage_manager.js
@@ -0,0 +1,98 @@
+window.fakeStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function LocalStorageManager() {
+  this.bestScoreKey     = "bestScore";
+  this.gameStateKey     = "gameState";
+  // Changes www.nitinpathak.esy.es
+  this.totalMovesKey    = "totalMoves";
+  this.lastMoveKey      = "move#";
+
+  var supported = this.localStorageSupported();
+  this.storage = supported ? window.localStorage : window.fakeStorage;
+}
+
+LocalStorageManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
+  var storage = window.localStorage;
+
+  try {
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// Best score getters/setters
+LocalStorageManager.prototype.getBestScore = function () {
+  return this.storage.getItem(this.bestScoreKey) || 0;
+};
+
+LocalStorageManager.prototype.setBestScore = function (score) {
+  this.storage.setItem(this.bestScoreKey, score);
+};
+
+// Game state getters/setters and clearing
+LocalStorageManager.prototype.getGameState = function () {
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+};
+
+LocalStorageManager.prototype.clearGameState = function () {
+  this.storage.removeItem(this.gameStateKey);
+};
+
+// Changes www.nitinpathak.esy.es
+LocalStorageManager.prototype.getLastMove = function (willUse) {
+  var i=this.getTotalMoves();
+  var stateJSON = this.storage.getItem(this.lastMoveKey+i.toString());
+  if (willUse && JSON.parse(stateJSON)) {
+    this.storage.removeItem(this.lastMoveKey+i.toString());
+    this.setTotalMoves(--i);
+  }
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.setLastMove = function (lastMove) {
+  var i=this.getTotalMoves();
+  this.storage.setItem(this.lastMoveKey+(++i).toString(), JSON.stringify(lastMove));
+  this.setTotalMoves(i);
+};
+
+LocalStorageManager.prototype.clearLastMoves = function () {
+  for (var i=this.getTotalMoves(); i > 0; i--) {
+    this.storage.removeItem(this.lastMoveKey+i.toString());
+  };
+  this.storage.removeItem(this.totalMovesKey);
+};
+
+LocalStorageManager.prototype.getTotalMoves = function () {
+  return this.storage.getItem(this.totalMovesKey) || 0;
+};
+
+LocalStorageManager.prototype.setTotalMoves = function (moves) {
+  this.storage.setItem(this.totalMovesKey, moves);
+};
